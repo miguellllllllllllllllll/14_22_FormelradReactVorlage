13eb6e597d85fbb3d6052eb8905aa54e89e98235
Miguel <152848745+Miguellllllllllllllllll@users.noreply.github.com>
2025-06-13 09:03:12 +0200

    Apply patch 0001: Encapsulate app in a new component

---
+++ Patches/04_AddPower/Ausgangslage/Formelrad.js
@@ -0,0 +1,28 @@
+import {useState} from "react";
+import '../css/mvp.css';
+import formelrad from "../image/formelradelektronik.gif";
+import InputField from "../formular/InputField";
+
+export default function Formelrad() {
+    const [values, setValues] = useState({
+        u: 10,
+        i: 2,
+        r: ""
+    })
+    return (
+        <>
+            <section>
+                <header>
+                    <h2>Formelrad</h2>
+                    <img src={formelrad} width="200" alt="Formelrad"/>
+                </header>
+                <form>
+                    <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
+                    <button type="submit">Calculate</button>
+                </form>
+            </section>
+        </>
+    )
+}
