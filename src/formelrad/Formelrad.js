9245baf850c6fe1007aa4427f50054f631f8c22b
Miguel <152848745+Miguellllllllllllllllll@users.noreply.github.com>
2025-06-13 14:19:20 +0200

    Apply patch 0024: If field is calculated text shall be red

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -23,53 +23,36 @@ export default function Formelrad() {
     event.preventDefault();
     console.log("handleSubmit");
 
-    const u = parseFloat(values.u);
-    const i = parseFloat(values.i);
-    const r = parseFloat(values.r);
-    const p = parseFloat(values.p);
-
-    if (isNaN(u) && isNaN(i)) {
-      // calculate u and i
-      setValues(values => ({
-        ...values,
-        u: Math.sqrt(p * r) || "",
-        i: Math.sqrt(p / r) || ""
-      }));
-    } else if (isNaN(u) && isNaN(r)) {
-      // calculate u and r
-      setValues(values => ({
-        ...values,
-        u: p / i || "",
-        r: p / (i * i) || ""
-      }));
-    } else if (isNaN(u) && isNaN(p)) {
-      // calculate u and p
-      setValues(values => ({
-        ...values,
-        u: i * r || "",
-        p: i * i * r || ""
-      }));
-    } else if (isNaN(i) && isNaN(r)) {
-      // calculate i and r
-      setValues(values => ({
-        ...values,
-        i: p / u || "",
-        r: (u * u) / p || ""
-      }));
-    } else if (isNaN(i) && isNaN(p)) {
-      // calculate i and p
-      setValues(values => ({
-        ...values,
-        i: u / r || "",
-        p: (u * u) / r || ""
-      }));
+    if (values.u === "" && values.i === "") {
+      /* calculate u and i */
+      setValues(values => ({ ...values, u: Math.sqrt(values.p * values.r) }));
+      setValues(values => ({ ...values, i: Math.sqrt(values.p / values.r) }));
+      setColors(colors => ({ ...colors, u: "red", i: "red" }));
+    } else if (values.u === "" && values.r === "") {
+      /* calculate u and r */
+      setValues(values => ({ ...values, u: values.p / values.i }));
+      setValues(values => ({ ...values, r: values.p / (values.i * values.i) }));
+      setColors(colors => ({ ...colors, u: "red", r: "red" }));
+    } else if (values.u === "" && values.p === "") {
+      /* calculate u and p */
+      setValues(values => ({ ...values, u: values.i * values.r }));
+      setValues(values => ({ ...values, p: values.i * values.i * values.r }));
+      setColors(colors => ({ ...colors, u: "red", p: "red" }));
+    } else if (values.i === "" && values.r === "") {
+      /* calculate i and r */
+      setValues(values => ({ ...values, i: values.p / values.u }));
+      setValues(values => ({ ...values, r: (values.u * values.u) / values.p }));
+      setColors(colors => ({ ...colors, i: "red", r: "red" }));
+    } else if (values.i === "" && values.p === "") {
+      /* calculate i and p */
+      setValues(values => ({ ...values, i: values.u / values.r }));
+      setValues(values => ({ ...values, p: (values.u * values.u) / values.r }));
+      setColors(colors => ({ ...colors, i: "red", p: "red" }));
     } else {
-      // calculate r and p
-      setValues(values => ({
-        ...values,
-        r: u / i || "",
-        p: u * i || ""
-      }));
+      /* calculate r and p */
+      setValues(values => ({ ...values, r: values.u / values.i }));
+      setValues(values => ({ ...values, p: values.u * values.i }));
+      setColors(colors => ({ ...colors, r: "red", p: "red" }));
     }
   };
 
